0ce856ea4edfb12bbd90c0be6b02adf1f622c1bf
Taylor Salo <tsalo006@fiu.edu>
2021-02-10 14:26:40 -0500

    Fix the environment?

--- create_conda_environment.sh
+++ create_conda_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 module add miniconda3-4.5.11-gcc-8.2.0-oqs2mbg
-conda create -n nimare_env pip python=3.7
-source activate -n nimare_env
-conda install -n nimare_env pip numpy scipy libgfortran sympy ply funcsigs cython matplotlib seaborn pandas numexpr scikit-learn tornado accelerate Biopython dateutil
-pip install pip -U
-pip install ipython jupyter-notebook ipywidgets nibabel nilearn
-pip install -e /home/data/nbc/misc-projects/Salo_NiMARE/NiMARE/
+conda create -p conda_env/ pip python=3.7
+source activate conda_env/
+conda install pip numpy scipy libgfortran sympy ply funcsigs cython matplotlib seaborn pandas numexpr scikit-learn tornado accelerate Biopython dateutil
+/home/data/nbc/misc-projects/Salo_NiMARE/nimare-paper/conda_env/bin/pip install pip -U
+/home/data/nbc/misc-projects/Salo_NiMARE/nimare-paper/conda_env/bin/pip install ipython jupyterlab ipywidgets nibabel nilearn
+/home/data/nbc/misc-projects/Salo_NiMARE/nimare-paper/conda_env/bin/pip install -e /home/data/nbc/misc-projects/Salo_NiMARE/NiMARE/
 conda list > python_requirements.txt
