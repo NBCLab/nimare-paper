2086b2dd270d2478dbee84d2219bf871197d096d
Taylor Salo <tsalo006@fiu.edu>
2021-03-18 12:49:52 -0400

    Use low_memory option for SCALE.

--- listings_figures_and_tables_part1.py
+++ listings_figures_and_tables_part1.py
@@ -133,6 +133,7 @@ ijk = ns_dset.coordinates[["i", "j", "k"]].values
 meta = nimare.meta.cbma.ale.SCALE(
     n_iters=10000,
     ijk=ijk,
+    low_memory=True,
     kernel__sample_size=20,
 )
 scale_results = meta.fit(sl_dset1)
