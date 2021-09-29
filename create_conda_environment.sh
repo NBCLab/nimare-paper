dd819e5eab30a2f167a2bcc5643811c7db414068
Taylor Salo <tsalo006@fiu.edu>
2021-09-29 13:35:40 -0400

    Move files and pin versions.

--- create_conda_environment.sh
+++ create_conda_environment.sh
@@ -4,4 +4,4 @@ conda create -p conda_env/ pip python=3.7
 source activate conda_env/
 conda install pip
 /home/data/nbc/misc-projects/Salo_NiMARE/nimare-paper/conda_env/bin/pip install pip -U
-/home/data/nbc/misc-projects/Salo_NiMARE/nimare-paper/conda_env/bin/pip install -r requirements.txt
+/home/data/nbc/misc-projects/Salo_NiMARE/nimare-paper/conda_env/bin/pip install -r binder/requirements.txt
