10b6bfc0e3aee6d6174868445961566a0d995469
Taylor Salo <tsalo006@fiu.edu>
2021-10-18 10:01:20 -0400

    Move folder up a level.

--- create_conda_environment.sh
+++ create_conda_environment.sh
@@ -3,5 +3,5 @@ module add miniconda3-4.5.11-gcc-8.2.0-oqs2mbg
 conda create -p conda_env/ pip python=3.7
 source activate conda_env/
 conda install pip
-/home/data/nbc/misc-projects/Salo_NiMARE/nimare-paper/conda_env/bin/pip install pip -U
-/home/data/nbc/misc-projects/Salo_NiMARE/nimare-paper/conda_env/bin/pip install -r binder/requirements.txt
+/home/data/nbc/misc-projects/Salo_NiMARE/conda_env/bin/pip install pip -U
+/home/data/nbc/misc-projects/Salo_NiMARE/conda_env/bin/pip install -r binder/requirements.txt
