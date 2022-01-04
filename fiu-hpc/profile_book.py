1b4d0b76536871fc89bfd508c14115377fc892aa
Taylor Salo <tsalo006@fiu.edu>
2022-01-04 14:03:31 -0500

    Need to convert to notebooks before profiling.

--- fiu-hpc/profile_book.py
+++ fiu-hpc/profile_book.py
@@ -1,4 +1,7 @@
-"""Execute each chapter in the Jupyter Book and measure memory usage."""
+"""Execute each chapter in the Jupyter Book and measure memory usage.
+
+I need to run `jupytext *.md --to ipynb` in ../content/ before this.
+"""
 import os
 from glob import glob
 
@@ -13,16 +16,16 @@ def run_page(filename):
         nb,
         timeout=600,
         kernel_name="python3",
-        resources={"metadata": {"path": "notebooks/"}},
+        resources={"metadata": {"path": "."}},
     )
     client.execute()
 
 
-chapters = sorted(glob("../contents/*.md"))
+chapters = sorted(glob("*.ipynb"))
 for chapter in chapters:
     print(os.path.basename(chapter))
     chapter_file = os.path.abspath(chapter)
 
-    mem_usage = memory_usage((run_page, chapter_file))
-    print(f"Memory usage (in chunks of .1 seconds): {mem_usage}")
-    print(f"Maximum memory usage: {max(mem_usage)}")
+    mem_usage = memory_usage((run_page, (chapter_file,)), interval=10, timeout=None)
+    print(f"Memory usage (in chunks of 10 seconds): {mem_usage}")
+    print(f"Maximum memory usage: {max(mem_usage)} MB")
