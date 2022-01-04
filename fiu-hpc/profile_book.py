2b8c6e6cf2715d2618abb04521f118c31584edff
Taylor Salo <tsalo006@fiu.edu>
2022-01-04 12:40:30 -0500

    Draft profiling script.

---
+++ fiu-hpc/profile_book.py
@@ -0,0 +1,28 @@
+"""Execute each chapter in the Jupyter Book and measure memory usage."""
+import os
+from glob import glob
+
+import nbformat
+from memory_profiler import memory_usage
+from nbclient import NotebookClient
+
+
+def run_page(filename):
+    nb = nbformat.read(filename, as_version=4)
+    client = NotebookClient(
+        nb,
+        timeout=600,
+        kernel_name="python3",
+        resources={"metadata": {"path": "notebooks/"}},
+    )
+    client.execute()
+
+
+chapters = sorted(glob("../contents/*.md"))
+for chapter in chapters:
+    chapter_file = os.path.abspath(chapter)
+
+    mem_usage = memory_usage((run_page, chapter_file))
+    print(os.path.basename(chapter_file))
+    print(f"Memory usage (in chunks of .1 seconds): {mem_usage}")
+    print(f"Maximum memory usage: {max(mem_usage)}")
