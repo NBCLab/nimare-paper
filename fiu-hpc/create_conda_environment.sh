bbdd9051e01b66982e637d0e11edc2583746f944
Taylor Salo <tsalo006@fiu.edu>
2022-09-07 17:15:08 -0400

    Add Simon Eickhoff as coauthor.

--- fiu-hpc/create_conda_environment.sh
+++ fiu-hpc/create_conda_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module add miniconda3-4.5.11-gcc-8.2.0-oqs2mbg
-conda create -p /home/data/nbc/misc-projects/Salo_NiMARE/conda_env/ pip python=3.7
+conda create -p /home/data/nbc/misc-projects/Salo_NiMARE/conda_env/ pip python=3.8
 source activate /home/data/nbc/misc-projects/Salo_NiMARE/conda_env/
 conda install pip
 /home/data/nbc/misc-projects/Salo_NiMARE/conda_env/bin/pip install pip -U
